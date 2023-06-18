27ed4be7ea7c1e06b331ca14a7b328ebfdc73f1a
ho <hohohosky101@gmail.com>
2023-06-18 15:12:29 +0900

    헤더 스크롤

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let eventBlock = false;
+
+window.addEventListener('scroll', () => {
+	const header = document.querySelector('#header');
+
+	if (eventBlock) return;
+
+	eventBlock = setTimeout(() => {
+		activeHeader(header);
+		eventBlock = false;
+	}, 300);
+});
+
+function activeHeader(header) {
+	const scroll = window.scrollY;
+	scroll > 0 ? (header.className = 'on') : (header.className = 'off');
+}
