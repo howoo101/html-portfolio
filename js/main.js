5625d26f7f86e92ffdaa79661099d8ab0599e638
ho <hohohosky101@gmail.com>
2023-06-25 16:27:44 +0900

    [feature]메인페이지 scroll navi

--- js/main.js
+++ js/main.js
@@ -1,17 +1,37 @@
+const secs = document.querySelectorAll('.naviScroll');
+const scrollButtons = document.querySelectorAll('#naviScroll li');
+const baseline = -window.innerHeight / 2;
 let eventBlock = false;
 
 window.addEventListener('scroll', () => {
-	const header = document.querySelector('#header');
-
 	if (eventBlock) return;
-
+	console.log(scroll);
 	eventBlock = setTimeout(() => {
-		activeHeader(header);
+		activeHeader();
+		handleScroll();
 		eventBlock = false;
 	}, 300);
 });
 
-function activeHeader(header) {
+scrollButtons.forEach((btn, idx) => {
+	btn.addEventListener('click', () => {
+		window.scrollTo({ top: secs[idx].offsetTop + baseline, behavior: 'smooth' });
+	});
+});
+
+function handleScroll() {
+	const scroll = window.scrollY;
+	secs.forEach((_, idx) => {
+		if (scroll >= secs[idx].offsetTop + baseline) {
+			for (const btn of scrollButtons) btn.classList.remove('on');
+			scrollButtons[idx].classList.add('on');
+		}
+	});
+}
+//scroll시 헤더에
+function activeHeader() {
+	const header = document.querySelector('#header');
+
 	const scroll = window.scrollY;
 	scroll > 0 ? (header.className = 'on') : (header.className = 'off');
 }
